dd3a4b0d6e240ac4ccb421d056649b7274a1c56e
Valquiria Perea <94647582+valpereag@users.noreply.github.com>
2021-11-20 13:37:53 -0500

    Add files via upload

---
+++ input output.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+// Link para repasar
+// https://www.w3schools.com/cpp/cpp_variables.asp
+
+int main()
+{
+    int x;
+    int y;
+    int z;
+    cin >> x;
+    cin >> y;
+    cin >> z;
+    cout << x + y + z;
+
+}
