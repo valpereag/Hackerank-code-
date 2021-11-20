b3bb5a9a7b3dca137426e500ce924e6cc4797008
Valquiria Perea <94647582+valpereag@users.noreply.github.com>
2021-11-20 12:56:46 -0500

    Add files via upload

---
+++ Creating variables.cpp.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+// Link para repasar
+// https://www.w3schools.com/cpp/cpp_variables.asp
+
+int main()
+{ 
+    int x = 3;
+    int y = 4;
+    int z = 7;
+    cout << x << endl;
+    cout << y <<endl;
+    cout << z << endl;
+    cout << x + y + z;
+
+}
